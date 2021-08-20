873bac9cf36ef31963874ad4450d2b29c7cf613f
Nguyen Tran <48101680+tgm043@users.noreply.github.com>
2021-08-20 11:40:47 -0600

    chess board with no functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,183 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick = {props.onClick}
+    >
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(64).fill(null),
+
+        xNext: true,
+      };
+      //white pieces
+      this.state.squares[63] = '\u2656';
+      this.state.squares[62] = '\u2658';
+      this.state.squares[61] = '\u2657';
+      this.state.squares[60] = '\u2654';
+      this.state.squares[59] = '\u2655';
+      this.state.squares[58] = '\u2657';
+      this.state.squares[57] = '\u2658';
+      this.state.squares[56] = '\u2656';
+      for (let i = 0; i < 8; ++i){
+        this.state.squares[i+48] = '\u2659';
+      }
+      //black pieces
+      this.state.squares[0] = '\u265c';
+      this.state.squares[1] = '\u265e';
+      this.state.squares[2] = '\u265d';
+      this.state.squares[3] = '\u265b';
+      this.state.squares[4] = '\u265a';
+      this.state.squares[5] = '\u265d';
+      this.state.squares[6] = '\u265e';
+      this.state.squares[7] = '\u265c';
+      for (let i = 0; i < 8; ++i){
+        this.state.squares[i+8] = '\u265f';
+      }
+  }
+
+
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xNext: !this.state.xNext,
+    });
+  }
+
+  renderSquare(i) {
+    return (
+    <Square
+      value={this.state.squares[[i]]}
+      onClick={() => this.handleClick(i)}
+    />
+    );
+  }
+
+  render() {
+    const status = 'Next player: ' + (this.state.xNext ? 'White' : 'Black');
+    var counter = 0;
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+          {this.renderSquare(counter++)}
+        </div>
+      </div>
+
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
